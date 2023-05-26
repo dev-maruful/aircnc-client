32269d6a3bc6fc85f795c22e77b06e7526b82ab0
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-05-26 20:05:12 +0600

    navbar creation done

---
+++ src/components/shared/Navbar/Avatar.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import avatarImg from "../../../assets/images/placeholder.jpg";
+import { AuthContext } from "../../../providers/AuthProvider";
+
+const Avatar = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <img
+      className="rounded-full"
+      src={user && user.photoURL ? user.photoURL : avatarImg}
+      alt="Avatar Image"
+      width="30"
+      height="30"
+    />
+  );
+};
+
+export default Avatar;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Main from '../layouts/Main'
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Main />,
   },
-])
+]);
